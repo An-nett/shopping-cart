c454b1e1e3519203bec6aa6d98949f88c224dc0e
Anya <sobina.ania@yandex.ru>
2021-04-03 03:13:28 +0300

    added total summ calculation

--- cart.js
+++ cart.js
@@ -29,8 +29,6 @@ $addBtn.click(function(e) {
     
     if (added.has(btnIndex)) {
 
-        console.log(btnIndex);
-
         let $multItem = $('.cart').find('.card-title');
 
         for (let i = 0; i < $multItem.length; i++) {
@@ -45,9 +43,6 @@ $addBtn.click(function(e) {
 
     else {
 
-        console.log(added);
-        console.log(btnIndex);
-
     let $chosenItem = $('<div></div>');
     
 
@@ -58,18 +53,49 @@ $addBtn.click(function(e) {
     let $textInfo = $('<div></div>').addClass('card-body d-flex flex-column')
                                     .appendTo($chosenItem);
 
-    $('<h4></h4>').text(items[btnIndex].name).addClass('card-title').appendTo($textInfo);
-    $('<p class="card-text"></p>').text(items[btnIndex].price).appendTo($textInfo).append('<span> x 1</span>');
-    $('<button[type="button"] class="btn delete btn-outline-primary">Delete</button>').appendTo($textInfo);
+    $('<h4></h4>')
+        .text(items[btnIndex].name)
+        .addClass('card-title')
+        .appendTo($textInfo);
+
+    $('<p class="card-text items-prices"></p>')
+        .text(items[btnIndex].price)
+        .appendTo($textInfo)
+
+        .append('<span class = "amount"> x 1</span>');
+    $('<button[type="button"] class="btn delete btn-outline-primary">Delete</button>')
+        .appendTo($textInfo);
 
 
-    $chosenItem.appendTo( '.cart' )
-               .addClass('card mb-1 d-flex flex-row justify-content-between border w-50 align-items-center'); 
+    if ($('.total').length == 1) {
+        $chosenItem.insertBefore('.total');
+    }
+
+    else {
+        $chosenItem.appendTo( '.cart' )
+    }
+   
+    $chosenItem.addClass('card mb-1 d-flex flex-row justify-content-between border w-50 align-items-center'); 
     
     };
 
 
     added.add(btnIndex);
+
+    if (($('.delete').length == 1) && ($('.amount').text() == " x 1") ) {
+
+        let $totSum = $('<div class = "total bg-secondary text-white"></div>').appendTo('.cart');
+
+        $('<p class = "fs-4 mb-0"> Total Sum: </p>')
+            .appendTo($totSum).append($('<span class = "result fst-italic"></span>'));
+
+        $('<p class = "fs-5 mb-0"> Taxes: </p>')
+            .appendTo($totSum).append($('<span class = "taxes fst-italic"></span>'));
+
+        $('<button[type="button"] class = "btn btn-light text-primary mt-1 mb-1 buy"> Buy Items </button>').appendTo($totSum);
+    }
+
+    calcSum();
 
 })
 
@@ -91,6 +117,26 @@ $('.cart').click(function(e) {
         }
 
     added.delete(delIndex);
+    calcSum();
 
     }  
 })
+
+function calcSum() {
+    let $prices = $('.items-prices');
+    let totalSum = 0;
+    for (let i = 0; i < $prices.length; i++) {
+        let calcPrice = $($prices[i]).text().split(' x ');
+        let result = calcPrice[0] * calcPrice[1];
+        totalSum += result;
+    }
+
+    let tax = + (totalSum * 0.1).toFixed(2);
+    totalSum = + totalSum.toFixed(2);
+
+    $('.result').text(totalSum + '$');
+    $('.taxes').text(tax + '$');
+
+}
+
+
