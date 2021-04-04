3cede93434723282b86e7d54d8b26989f6c5a98a
Anya <sobina.ania@yandex.ru>
2021-04-04 04:07:34 +0300

    started submitting form

--- cart.js
+++ cart.js
@@ -6,6 +6,9 @@ let $titles = $( '.card' ).find( '.card-title' );
 let $prices = $( '.card' ).find( '.card-text' );
 let $images = $( '.card' ).find( '.img-fluid' );
 
+
+
+//построение массива с данными из карточек товаров
 function makeArray() {
   for (let i = 0; i < $( '.card' ).length; i++) {
       let item = {
@@ -19,14 +22,17 @@ function makeArray() {
 
 makeArray();
 
+
+//сет с индексами тех карточек, которые лежат в корзине
 let added = new Set();
 
 
+//функционал на кнопку добавить в корзину
 $addBtn.click(function(e) {
 
     let btnIndex = $.inArray(e.target, $addBtn);
 
-    
+    //если уже есть в корзине (умножение)
     if (added.has(btnIndex)) {
 
         let $multItem = $('.cart').find('.card-title');
@@ -34,15 +40,17 @@ $addBtn.click(function(e) {
         for (let i = 0; i < $multItem.length; i++) {
 
             if ($multItem.eq(i).text() == items[btnIndex].name) {
-                let $amount = $multItem.eq(i).next().find('span');
+                let $amount = $multItem.eq(i).next().find('.amount');
                 let numAmount = + $amount.text().slice(3);
                 $amount.text(' x ' + (numAmount + 1));
             }
         }
     }
 
+    //если добавляется в первый раз
     else {
 
+    //карточка с товаром
     let $chosenItem = $('<div></div>');
     
 
@@ -50,23 +58,34 @@ $addBtn.click(function(e) {
               .addClass('img-fluid h-50')
               .appendTo($chosenItem);
 
+    
+    //текстовая часть
     let $textInfo = $('<div></div>').addClass('card-body d-flex flex-column')
                                     .appendTo($chosenItem);
 
+    //заголовок
     $('<h4></h4>')
         .text(items[btnIndex].name)
         .addClass('card-title')
         .appendTo($textInfo);
 
-    $('<p class="card-text items-prices"></p>')
+    //цена
+    $('<p class="card-text items-prices mb-0"></p>')
         .text(items[btnIndex].price)
         .appendTo($textInfo)
-
         .append('<span class = "amount"> x 1</span>');
+
+    //плюс и минус
+    $('<p class = "amount-change mb-0"></p>')
+        .append('<a href="#" class = "plus link-success fs-1 text-decoration-none"> + </a>')
+        .append('<a href="#" class = "minus link-danger fs-1 text-decoration-none"> - </a>')
+        .appendTo($textInfo);
+
+    //кнопка удалить
     $('<button[type="button"] class="btn delete btn-outline-primary">Delete</button>')
         .appendTo($textInfo);
 
-
+    //куда добавить (перед общей суммой или впервые)
     if ($('.total').length == 1) {
         $chosenItem.insertBefore('.total');
     }
@@ -82,6 +101,8 @@ $addBtn.click(function(e) {
 
     added.add(btnIndex);
 
+
+    //если еще нет карточки с общей суммой
     if (($('.delete').length == 1) && ($('.amount').text() == " x 1") ) {
 
         let $totSum = $('<div class = "total bg-secondary text-white"></div>').appendTo('.cart');
@@ -97,31 +118,45 @@ $addBtn.click(function(e) {
 
     calcSum();
 
+
 })
 
+//функционал для кнопок удаления
 $('.cart').click(function(e) {
    
     if ($(e.target).attr('class').includes('delete')) {
+        deleteCard(e.target);
+    }  
+})
 
-        $(e.target).closest('div.card').remove();
+//функция для удаления карточки из корзины
+function deleteCard(target) {
 
-        let name = $(e.target).siblings('h4').text();
-        let delIndex;
+    $(target).closest('div.card').remove();
 
-        for (let i = 0; i < items.length; i++) {
+    let name = $(target).siblings('h4').text();
+    let delIndex;
 
-            if (name == items[i].name) {
-                delIndex = i;
-            }
+    for (let i = 0; i < items.length; i++) {
 
+        if (name == items[i].name) {
+            delIndex = i;
         }
+
+    }
+
+//удаляет карточку с общей суммой, если корзина пустая
+    if ($('.delete').length < 1) {
+        $('.total').remove();
+    }
 
     added.delete(delIndex);
     calcSum();
 
-    }  
-})
+}
 
+
+//расчет общей суммы покупки
 function calcSum() {
     let $prices = $('.items-prices');
     let totalSum = 0;
@@ -139,4 +174,26 @@ function calcSum() {
 
 }
 
+//функционал кнопок с плюсом и минусом
+$('.cart').click(function(e) {
+
+    if ($(e.target).hasClass('plus')) {
+        let $toChange = $(e.target).parent().siblings('.card-text').find('.amount');
+        let curVal = + $toChange.text().slice(3);
+        $toChange.text(' x ' + (curVal + 1));
+    }
+
+    if ($(e.target).hasClass('minus')) {
+        let $toChange = $(e.target).parent().siblings('.card-text').find('.amount');
+        let curVal = + $toChange.text().slice(3);
+        if (curVal > 1) {
+            $toChange.text(' x ' + (curVal - 1));
+        }
+        else {
+            deleteCard(e.target);
+        }
+       
+    }
+    calcSum();
+})
 
